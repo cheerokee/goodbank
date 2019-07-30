3a85373d39e14d7e23ca1c3d3f0272493d05682d
Jonas Inhani Martins <jonasmweb@hotmail.com>
2019-07-30 11:12:03 -0300

    Alterações

--- module/Base/src/Base/Controller/CrudController.php
+++ module/Base/src/Base/Controller/CrudController.php
@@ -239,7 +239,7 @@ abstract class CrudController extends AbstractActionController{
                 foreach ($form as $element) {
                     $type = $element->getAttributes()['type'];
                     if($type == 'date' || $type == 'datetime') {
-
+                        str_replace('T',' ',$data[$element->getName()]);
                         $data[$element->getName()] = new \DateTime($data[$element->getName()]);
                         var_dump($data[$element->getName()]);
                         die;
